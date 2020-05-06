02bf556adb1ecaa932fa53dece60eb49ffe418c5
Platosdog <62915572+Platosdog@users.noreply.github.com>
2020-05-05 19:03:18 -0500

    initial code

---
+++ site.js
@@ -0,0 +1,9 @@
+
+function getPictures(){
+
+    var values = ["test","values","go","here"],
+    valueToUse = values[Math.floor(Math.random() * values.length)];
+// do something with the selected value
+alert(valueToUse);
+
+}
